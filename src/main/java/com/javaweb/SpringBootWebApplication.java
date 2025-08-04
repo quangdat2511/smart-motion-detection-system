b30976841d07d6ec98e26b42d60ccf1254a4f818
QuangDat <ngodat25112005@gmail.com>
2025-08-04 11:17:37 +0700

    config Firebase

--- src/main/java/com/javaweb/SpringBootWebApplication.java
+++ src/main/java/com/javaweb/SpringBootWebApplication.java
@@ -1,40 +1,23 @@
-package com.javaweb;
+    package com.javaweb;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import java.io.File;
-import java.io.FileInputStream;
+    import org.springframework.boot.SpringApplication;
+    import org.springframework.boot.autoconfigure.SpringBootApplication;
+    import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+    import org.springframework.boot.builder.SpringApplicationBuilder;
+    import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class SpringBootWebApplication extends SpringBootServletInitializer {
+    @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+    public class SpringBootWebApplication extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SpringBootWebApplication.class);
+        @Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+            return application.sources(SpringBootWebApplication.class);
+        }
+
+        public static void main(String[] args) {
+            SpringApplication.run(SpringBootWebApplication.class, args);
+        }
     }
-
-    public static void main(String[] args) throws Exception {
-        ClassLoader classLoader = SpringBootWebApplication.class.getClassLoader();
-
-        File file = new File(classLoader.getResource("serviceAccountKey.json").getFile());
-        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
-
-        FirebaseApp.initializeApp(options);
-
-        SpringApplication.run(SpringBootWebApplication.class, args);
-    }
-}
 
 
