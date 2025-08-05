d63a1ffd2c031d735a07d74343d7fc4cb8bcc54b
QuangDat <ngodat25112005@gmail.com>
2025-08-05 17:46:36 +0700

    Reset tracking to match new .gitignore rules

--- src/main/java/com/javaweb/exception/ValidateDataException.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.exception;
-
-public class ValidateDataException extends RuntimeException{
-    public ValidateDataException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/javaweb/service/MqttService.java
+++
@@ -1,7 +0,0 @@
-package com.javaweb.service;
-
-public interface MqttService {
-    void publishBuzzerMessage(String message);
-    void publishLcdMessage(String message);
-    void publishServoMessage(String message);
-}
